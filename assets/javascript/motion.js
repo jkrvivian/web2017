a388f4c3be9856c1fa1f088751053001355296d0
jkrvivian <jkrvivian@gmail.com>
2017-09-26 21:28:18 +0800

    Modify button toggle message

--- assets/javascript/motion.js
+++ assets/javascript/motion.js
@@ -1,8 +1,11 @@
 function showBBT() {
   var x = document.getElementById('bbt');
+  var but = document.getElementById('but_hob');
   if (x.style.display === 'none') {
       x.style.display = 'block';
+      but.value = "Click me to close";
   } else {
       x.style.display = 'none';
+      but.value = "Click me to find out!";
   }
 }
