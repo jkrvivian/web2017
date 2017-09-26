e8f57542850b3928c0c20268841c289abfd6468c
jkrvivian <jkrvivian@gmail.com>
2017-09-26 21:07:42 +0800

    Add profile picture, education and hobby

---
+++ assets/javascript/motion.js
@@ -0,0 +1,8 @@
+function showBBT() {
+  var x = document.getElementById('bbt');
+  if (x.style.display === 'none') {
+      x.style.display = 'block';
+  } else {
+      x.style.display = 'none';
+  }
+}
